033ced1808483a806e53479626b8bc07f0af113d
wa86314 <2021wa86314@wilp.bits-pilani.ac.in>
2025-10-12 19:48:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ fill_mask = pipeline("fill-mask", model="bert-base-uncased")
 
 @app.get("/")
 def root():
-    return {"status": "running"}
+    return {"status": "UP"}
 
 @app.get("/predict",response_class=HTMLResponse)
 def predict(text: str):
